a584db056766fe17d3c758efa6a88a9eae11a528
ariran5 <Arian@MacBook-Air-Arian.local>
2016-10-26 05:54:27 +0300

    secondCommit
    Изменена формула отрисовки svg для возможности применить 1 формулу ко
    всем svg

--- js/common.js
+++ js/common.js
@@ -58,61 +58,33 @@ var sizes = function(){
 }();
 
 
-
-
-var svg1 = document.getElementById('svg_1');
-svg1.addEventListener('click',drawImage);
-function drawImage() {
-	var pathLength =  window.getComputedStyle(this).strokeDasharray;
-	var path = this;
-	var offset = parseInt(pathLength);
-	console.log(offset);
-
-	drawSVG();
-	function drawSVG() {
-	
-		if (offset > 0) {
-			path.style.strokeDashoffset = offset;
-			offset -=7;
-			requestAnimationFrame(drawSVG,60)
+var svgIds = function() {
+	var a = document.getElementsByTagName('svg');
+	var svgIds = [];
+	for (var q = 0; q < a.length; q++) {
+		if (!a[q].hasAttribute('data-svg-animation')) {continue;}
+		var svgId = a[q].getAttribute('id');
+		svgIds[q] = svgId;
+		var path = a[q].querySelectorAll('path');
+		for (var i = 0; i < path.length; i++) {
+			var a = path[i].getTotalLength();
+			path[i].style.strokeDasharray = a;
+			path[i].style.strokeDashoffset = a;
 		}
 	}
+	return svgIds;
+}();
 
-}
+var coordinatiesSvgDrawing = function() {
 
-
-
-
-function scrollin() {
-	var svg = document.getElementById('svg_0');
-	var path = document.getElementById('svg_1');
-	var pathLength = path.getTotalLength();
-	var scrolled = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
-	var coord = svg.getBoundingClientRect();
-	var w = coord.top;
-	var h = coord.bottom;
-	var windowOnStartScroll = sizes.windowHeight/4;
-
-
-	var scrollStart = scrolled + w - windowOnStartScroll;
-	var scrollEnd = scrolled + h - windowOnStartScroll*3;
-
-
-	var scrolledLength = scrollEnd - scrollStart;
-	var punkt = pathLength/scrolledLength;
-	var as = pathLength - (scrolled - scrollStart) * punkt;
-	if (scrolled < scrollStart) path.style.strokeDashoffset = pathLength;
-	if (scrolled > scrollStart && scrolled < scrollEnd) {
-		path.style.strokeDashoffset = as;
-
-	}
-}
+};
 
 
 class svgScrollDrawing {
-	constructor() {
-		this.svg = document.getElementById('svg_3');
-		this.pathCount = document.querySelectorAll('#svg_3 path');
+	constructor(svgId) {
+		this.svgId = svgId;
+		this.svg = document.getElementById(this.svgId);
+		this.pathCount = this.svg.querySelectorAll('path');
 		for (var i = 0; i < this.pathCount.length; i++) {
 			this._dataInit(i);
 			this._calculate();
@@ -143,13 +115,56 @@ class svgScrollDrawing {
 	}
 }
 
-var path = document.querySelectorAll('#svg_3 path');
-for (var i = 0; i < path.length; i++) {
-	var a = path[i].getTotalLength();
-	path[i].style.strokeDasharray = a;
-	path[i].style.strokeDashoffset = a;
+
+window.onscroll = function() {
+	//new svgScrollDrawing('tree');
 }
 
-window.onscroll = function() { 
-	new svgScrollDrawing();
-}
+
+
+
+// var svg1 = document.getElementById('svg_1');
+// svg1.addEventListener('click',drawImage);
+
+// function drawImage() {
+// 	var pathLength =  window.getComputedStyle(this).strokeDasharray;
+// 	var path = this;
+// 	var offset = parseInt(pathLength);
+// 	console.log(offset);
+
+// 	drawSVG();
+// 	function drawSVG() {
+	
+// 		if (offset > 0) {
+// 			path.style.strokeDashoffset = offset;
+// 			offset -=7;
+// 			requestAnimationFrame(drawSVG,60)
+// 		}
+// 	}
+
+// }
+
+// function scrollin() {
+// 	var svg = document.getElementById('svg_0');
+// 	var path = document.getElementById('svg_1');
+// 	var pathLength = path.getTotalLength();
+// 	var scrolled = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+// 	var coord = svg.getBoundingClientRect();
+// 	var w = coord.top;
+// 	var h = coord.bottom;
+// 	var windowOnStartScroll = sizes.windowHeight/4;
+
+
+// 	var scrollStart = scrolled + w - windowOnStartScroll;
+// 	var scrollEnd = scrolled + h - windowOnStartScroll*3;
+
+
+// 	var scrolledLength = scrollEnd - scrollStart;
+// 	var punkt = pathLength/scrolledLength;
+// 	var as = pathLength - (scrolled - scrollStart) * punkt;
+// 	if (scrolled < scrollStart) path.style.strokeDashoffset = pathLength;
+// 	if (scrolled > scrollStart && scrolled < scrollEnd) {
+// 		path.style.strokeDashoffset = as;
+
+// 	}
+// }
